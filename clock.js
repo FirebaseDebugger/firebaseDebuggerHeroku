9f76750863755cafe7f8598ced231317e2c8fb05
intern.hautecouture <intern.hautecouture@gmail.com>
2018-11-02 16:54:10 +0900

    clock.js

---
+++ clock.js
@@ -0,0 +1,25 @@
+const firebase = require("firebase/app");
+require("firebase/database");
+const cron = require("cron").CronJob;
+
+var config = {
+    apiKey: "AIzaSyB5gymhz9lDbjRyo4UaBKpEknqJsej2tyg",
+    authDomain: "rankingtest-4a870.firebaseapp.com",
+    databaseURL: "https://rankingtest-4a870.firebaseio.com",
+    projectId: "rankingtest-4a870",
+    storageBucket: "rankingtest-4a870.appspot.com",
+    messagingSenderId: "352458038722"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+var job = new cron("* 55 18 * * *", function()
+{
+	db.ref("/HC_GAMES/").remove();
+	
+},null, true, "Asia/Tokyo"
+);
+
+job.start();
--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 //const functions = require('firebase-functions');
-const firebase = require("firebase/app");
-require("firebase/database");
-const cron = require("cron").CronJob;
 const http = require('http');
-
-var config = {
-    apiKey: "AIzaSyB5gymhz9lDbjRyo4UaBKpEknqJsej2tyg",
-    authDomain: "rankingtest-4a870.firebaseapp.com",
-    databaseURL: "https://rankingtest-4a870.firebaseio.com",
-    projectId: "rankingtest-4a870",
-    storageBucket: "rankingtest-4a870.appspot.com",
-    messagingSenderId: "352458038722"
-};
-
-firebase.initializeApp(config);
-
-var db = firebase.database();
-
-var job = new cron("* 48 16 * * *", function()
-{
-	db.ref("/HC_GAMES/").remove();
-	
-},null, true, "Asia/Tokyo"
-);
-
-job.start();
 
 http.createServer(function (request, res) {
 	console.log("server");
